dd5ef159e548cc01af51a43694b6a4def02ddd70
Chiu Eric <c3366301@gmail.com>
2017-02-18 13:45:13 +0800

    add one-to-many Attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 
+  has_many :attendees
+
   validates_presence_of :name
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216054256) do
+ActiveRecord::Schema.define(version: 20170217101304) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
