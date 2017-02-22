1718927dc49ab30b4ddf8e6f073e2998b66ecb86
Chiu Eric <c3366301@gmail.com>
2017-02-22 20:40:02 +0800

    add fake datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Group.create( :name => "Ruby")
+Group.create( :name => "JavaScript")
+Group.create( :name => "Java")
