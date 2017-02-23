4616b589e39076823b798082540bc770c295c167
Chiu Eric <c3366301@gmail.com>
2017-02-23 10:47:31 +0800

    simple authentica

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 
-  before_action :authenticate_user!, expect: [:index]
+  before_action :authenticate_user!, except: [:index]
 
   before_action :set_event, :only => [:show, :edit, :update, :destroy, :dashboard]
 
